e278550a99c1ec1cbec42ec24bc410a6669e15c3
spathak072 <saurabhpathak072@gmail.com>
2025-08-18 00:08:34 +0530

    Updated pricing page

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,5 +1,66 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+
+
+import { Link as RouterLink } from 'react-router-dom';
+import { Box, Button, Card, CardActions, CardContent, CardHeader, Container, Grid, Link, makeStyles, Typography } from '@mui/material';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+// const useStyles = makeStyles((theme) => ({
+//   '@global': {
+//     ul: {
+//       margin: 0,
+//       padding: 0,
+//       listStyle: 'none',
+//     },
+//   },
+//   toolbar: {
+//     flexWrap: 'wrap',
+//   },
+//   toolbarTitle: {
+//     flexGrow: 1,
+//   },
+//   link: {
+//     margin: theme.spacing(1, 1.5),
+//   },
+//   heroContent: {
+//     padding: theme.spacing(8, 0, 6),
+//   },
+//   cardHeader: {
+//     backgroundColor:
+//       theme.palette.type === 'light'
+//         ? theme.palette.grey[200]
+//         : theme.palette.grey[700],
+//   },
+//   cardPricing: {
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'baseline',
+//     marginBottom: theme.spacing(2),
+//   },
+//   footer: {
+//     borderTop: `1px solid ${theme.palette.divider}`,
+//     marginTop: theme.spacing(8),
+//     paddingTop: theme.spacing(3),
+//     paddingBottom: theme.spacing(3),
+//     [theme.breakpoints.up('sm')]: {
+//       paddingTop: theme.spacing(6),
+//       paddingBottom: theme.spacing(6),
+//     },
+//   },
+// }));
 
 const tiers = [
   {
@@ -71,17 +132,126 @@ const footers = [
 ];
 
 export default function Pricing() {
+  // const classes = useStyles();
+
   return (
     <React.Fragment>
-      <h1>Pricing</h1>
-      <Link to="/">
-        <button
-          variant="contained"
-          color="primary"
+      {/* Hero unit */}
+      <Container maxWidth="sm" component="main" 
+      // className={classes.heroContent}
+      >
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+          gutterBottom
         >
-          Landing
-        </button>
-      </Link>
+          Pricing
+        </Typography>
+        <Typography
+          variant="h5"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Quickly build an effective pricing table for your potential customers
+          with this layout. It&apos;s built with default Material-UI components
+          with little customization.
+        </Typography>
+      </Container>
+      {/* End hero unit */}
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={5} alignItems="flex-end">
+          {tiers.map((tier) => (
+            // Enterprise card is full width at sm breakpoint
+            <Grid
+              item
+              key={tier.title}
+              xs={12}
+              sm={tier.title === 'Enterprise' ? 12 : 6}
+              md={4}
+            >
+              <Card>
+                <CardHeader
+                  title={tier.title}
+                  subheader={tier.subheader}
+                  titleTypographyProps={{ align: 'center' }}
+                  subheaderTypographyProps={{ align: 'center' }}
+                  action={tier.title === 'Pro' ? 'star' : null}
+                  // className={classes.cardHeader}
+                />
+                <CardContent>
+                  <div 
+                  // className={classes.cardPricing}
+                  >
+                    <Typography component="h2" variant="h3" color="textPrimary">
+                      ${tier.price}
+                    </Typography>
+                    <Typography variant="h6" color="textSecondary">
+                      /mo
+                    </Typography>
+                  </div>
+                  <ul>
+                    {tier.description.map((line) => (
+                      <Typography
+                        component="li"
+                        variant="subtitle1"
+                        align="center"
+                        key={line}
+                      >
+                        {line}
+                      </Typography>
+                    ))}
+                  </ul>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    component={RouterLink}
+                    to="/auth/signup"
+                    fullWidth
+                    color="primary"
+                  >
+                    {tier.buttonText}
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      {/* Footer */}
+      <Container maxWidth="md" component="footer" 
+      // className={classes.footer}
+      >
+        <Grid container spacing={4} justifyContent="space-evenly">
+          {footers.map((footer) => (
+            <Grid item xs={6} sm={3} key={footer.title}>
+              <Typography variant="h6" color="textPrimary" gutterBottom>
+                {footer.title}
+              </Typography>
+              <ul>
+                {footer.description.map((item) => (
+                  <li key={item}>
+                    <Link href="#" variant="subtitle1" color="textSecondary">
+                      {item}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </Grid>
+          ))}
+        </Grid>
+        <Box mt={5}>
+          <RouterLink to="/">
+            <Button variant="contained" color="primary">
+              Back to Home
+            </Button>
+          </RouterLink>
+          <Copyright />
+        </Box>
+      </Container>
+      {/* End footer */}
     </React.Fragment>
   );
 }
